3201adc7fc2bcd2b5f4e5af03ea08349e44149c7
David Cano <david.cano@gruposolitium.es>
2022-03-23 00:23:29 +0100

    push actualizadas app

--- hello-world-python/launch.py
+++ hello-world-python/launch.py
@@ -4,4 +4,4 @@ helloworld = Flask(__name__)
 def run():
     return "{\"message\":\"Hello World Python www.davidcanoteayuda.com v1\"}"
 if __name__ == "__main__":
-    helloworld.run(host="0.0.0.0", port=int("5555"), debug=True)
+    helloworld.run(host="0.0.0.0", port=int("5000"), debug=True)
